2e8df4dcbb00397f90f1d6a9f4a64f42187967c3
danieleksantos <danimistron@gmail.com>
2025-07-03 14:57:22 +0000

    implementação

---
+++ src/main/java/com/deliverytech/delivery/entity/StatusPedido.java
@@ -0,0 +1,13 @@
+package com.deliverytech.enums;
+package com.deliverytech.delivery.entity;
+
+
+public enum StatusPedido {
+   PENDENTE,
+   CONFIRMADO,
+   PREPARANDO,
+   SAIU_PARA_ENTREGA,
+   ENTREGUE,
+   CANCELADO
+
+} 
